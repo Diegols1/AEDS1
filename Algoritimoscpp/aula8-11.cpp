96a3fcf48fc5f8bd4c8807dd316117af1b77e8ed
4snt <140409336+4snt@users.noreply.github.com>
2023-11-08 21:30:15 -0300

    Atualizar o aula8-11.cpp

--- Algoritimoscpp/aula8-11.cpp
+++ Algoritimoscpp/aula8-11.cpp
@@ -1,1 +1,20 @@
-a
+include<iostream>
+	using namespace std;
+	int main(){
+	int v[10],posmaior,posmenor,maior,menor;
+	menor=2147483647;
+    maior=2147483647;
+    for(int i=0; i<10; i++){
+        cout<< "digite o valor\n";
+        cin >> v[i];
+        if(v[i]>maior){
+            maior=v[i];
+            posmaior=i;
+        }
+        if(v[i]<menor){
+            menor=v[i];
+            posmenor=i;
+        }
+       }
+    return0
+    }
