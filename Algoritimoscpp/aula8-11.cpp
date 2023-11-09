19cc22c6a91ca616826f47a732ee31cfa28afca3
4snt <140409336+4snt@users.noreply.github.com>
2023-11-08 21:36:29 -0300

    aula8-11.cpp

--- Algoritimoscpp/aula8-11.cpp
+++ Algoritimoscpp/aula8-11.cpp
@@ -1,20 +1,41 @@
-include<iostream>
-	using namespace std;
-	int main(){
-	int v[10],posmaior,posmenor,maior,menor;
-	menor=2147483647;
-    maior=2147483647;
-    for(int i=0; i<10; i++){
-        cout<< "digite o valor\n";
-        cin >> v[i];
-        if(v[i]>maior){
-            maior=v[i];
-            posmaior=i;
-        }
-        if(v[i]<menor){
-            menor=v[i];
-            posmenor=i;
-        }
-       }
-    return0
-    }
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int op;
+    string sn;
+
+    while (true) { // Loop to repeat the operation
+        op = 0;
+
+        cout << "-----------------------------------------------\n";
+        cout << "Bem Vindo a calculadora, c++ escolha a operação\n";
+        cout << "-----[1] - Soma -------------------------------\n";
+        cout << "-----[2] - Subtração---------------------------\n";
+        cout << "-----[3]---------------------------------------\n";
+        cout << "-----[4] - Multiplicação-----------------------\n";
+        cout << "-----------------------------------------------\n";
+        cin >> op;
+
+        switch (op) {
+            case 1:
+            case 2:
+            case 3:
+            int main(){
+            int v[10],posmaior,posmenor,maior,menor;
+           menor=2147483647;
+           maior=2147483647;
+           for(int i=0; i<10; i++){
+                cout<< "digite o valor\n";
+                cin >> v[i];
+                if(v[i]>maior){
+                    maior=v[i];
+                    posmaior=i;
+                }
+                if(v[i]<menor){
+                    menor=v[i];
+                    posmenor=i;
+                }
+           }
+    
