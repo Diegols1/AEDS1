e0798fd8b64643b0c69a2e3f841a732ed09b773a
4snt <murilo.escobedo@protonmail.com>
2023-12-12 19:33:08 -0300

    Atualização das variaveis do trabalho final de atila

--- Algoritimoscpp/teste.cpp
+++ Algoritimoscpp/teste.cpp
@@ -4,30 +4,30 @@
 
 using namespace std;
 
-const int SIZE = 6;
+const int tamanho = 6;
 
-void preencherMatriz(int matriz[][SIZE]) {
+void preencherMatriz(int matriz[][tamanho]) {
     srand(time(0));
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
+    for (int i = 0; i < tamanho; ++i) {
+        for (int j = 0; j < tamanho; ++j) {
             matriz[i][j] = rand() % 2;  // Preenche aleatoriamente com 0 ou 1
         }
     }
 }
 
-void imprimirMatriz(int matriz[][SIZE]) {
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
+void imprimirMatriz(int matriz[][tamanho]) {
+    for (int i = 0; i < tamanho; ++i) {
+        for (int j = 0; j < tamanho; ++j) {
             cout << matriz[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-int contarMinasVizinhas(int matriz[][SIZE], int row, int col) {
+int contarMinasVizinhas(int matriz[][tamanho], int row, int col) {
     int count = 0;
-    for (int i = max(0, row - 1); i <= min(SIZE - 1, row + 1); ++i) {
-        for (int j = max(0, col - 1); j <= min(SIZE - 1, col + 1); ++j) {
+    for (int i = max(0, row - 1); i <= min(tamanho - 1, row + 1); ++i) {
+        for (int j = max(0, col - 1); j <= min(tamanho - 1, col + 1); ++j) {
             if (i != row || j != col) {
                 count += matriz[i][j];
             }
@@ -37,7 +37,7 @@ int contarMinasVizinhas(int matriz[][SIZE], int row, int col) {
 }
 
 int main() {
-    int campoMinado[SIZE][SIZE];
+    int campoMinado[tamanho][tamanho];
 
     char continuar;
     do {
@@ -47,8 +47,8 @@ int main() {
         imprimirMatriz(campoMinado);
 
         cout << "\nQuantidade de minas nas vizinhanças dos elementos não minados:" << endl;
-        for (int i = 0; i < SIZE; ++i) {
-            for (int j = 0; j < SIZE; ++j) {
+        for (int i = 0; i < tamanho; ++i) {
+            for (int j = 0; j < tamanho; ++j) {
                 if (campoMinado[i][j] == 0) {
                     cout << "a[" << i << "][" << j << "]: " << contarMinasVizinhas(campoMinado, i, j) << " minas" << endl;
                 }
