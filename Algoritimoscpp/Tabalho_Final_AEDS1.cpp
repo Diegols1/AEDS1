20211c6ab8a7d645afb80705571ca6d35708d5e9
4snt <murilo.escobedo@protonmail.com>
2023-12-12 19:58:48 -0300

    Atualização do codigo comentando

--- Algoritimoscpp/Tabalho_Final_AEDS1.cpp
+++ Algoritimoscpp/Tabalho_Final_AEDS1.cpp
@@ -1,11 +1,15 @@
+// Trabalho Final AEDS1
+// Professor: Áthila Rocha Trindade
+//Alunos: Murilo Santiago,  ,
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-const int tamanho = 6;
+const int tamanho = 6; 
 
+// Função para preencher a matriz com valores aleatórios (0 ou 1)
 void preencherMatriz(int matriz[][tamanho]) {
     srand(time(0));
     for (int i = 0; i < tamanho; ++i) {
@@ -15,6 +19,7 @@ void preencherMatriz(int matriz[][tamanho]) {
     }
 }
 
+// Função para imprimir a matriz
 void imprimirMatriz(int matriz[][tamanho]) {
     for (int i = 0; i < tamanho; ++i) {
         for (int j = 0; j < tamanho; ++j) {
@@ -24,8 +29,10 @@ void imprimirMatriz(int matriz[][tamanho]) {
     }
 }
 
+// Função para contar o número de minas nas vizinhanças de uma posição na matriz
 int contarMinasVizinhas(int matriz[][tamanho], int row, int col) {
     int count = 0;
+    // Loop para percorrer as posições vizinhas
     for (int i = max(0, row - 1); i <= min(tamanho - 1, row + 1); ++i) {
         for (int j = max(0, col - 1); j <= min(tamanho - 1, col + 1); ++j) {
             if (i != row || j != col) {
@@ -41,11 +48,14 @@ int main() {
 
     char continuar;
     do {
+        // Preenche a matriz com valores aleatórios
         preencherMatriz(campoMinado);
 
+        // Imprime a matriz
         cout << "Campo Minado:" << endl;
         imprimirMatriz(campoMinado);
 
+        // Calcula e imprime a quantidade de minas nas vizinhanças dos elementos não minados
         cout << "\nQuantidade de minas nas vizinhanças dos elementos não minados:" << endl;
         for (int i = 0; i < tamanho; ++i) {
             for (int j = 0; j < tamanho; ++j) {
@@ -55,6 +65,7 @@ int main() {
             }
         }
 
+        // Pergunta se o usuário deseja continuar
         cout << "\nDeseja continuar (s/n)? ";
         cin >> continuar;
 
@@ -62,4 +73,3 @@ int main() {
 
     return 0;
 }
-    
